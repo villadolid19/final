90ab4d390dc8889e9477f5d28845e64d15b9d3d7
zyn <villadolidmae@gmail.com>
2024-06-21 08:35:54 +0800

    Project Ongoing

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Projects.css';
 
+import Login from "../Assets/Log-in.jpg";
+import Dash from "../Assets/Dashboard.jpg";
+import Anal from "../Assets/Analytics.jpg";
+import Rept from "../Assets/Report.jpg";
+
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+
 const Projects = () => {
   return (
     <div className="Projects" id="Projects">
@@ -9,14 +16,22 @@ const Projects = () => {
         <h1 className="">Projects</h1>
 
         <div className="proj-sec">
-          <div className="proj-div"></div>
+
+          <div className="proj-div">
+            <img src={Login} alt="" />
+            <div className="info">
+              <h2>BISU Alumni Tracer System</h2>
+              <FaArrowUpRightFromSquare className="icon"/>
+            </div>
+          </div>
+
           <div className="proj-div"><h2>Upcoming</h2></div>
+          <div className="proj-div"><h2>Upcoming</h2></div>
+          <div className="proj-div"><h2>Upcoming</h2></div>
+
         </div>
 
-        <div className="proj-sec">
-          <div className="proj-div"><h2>Upcoming</h2></div>
-          <div className="proj-div"><h2>Upcoming</h2></div>
-        </div>
+        
 
       </div>
     </div>
